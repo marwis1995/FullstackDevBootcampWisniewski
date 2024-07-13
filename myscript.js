3f0ea6357cd55ae82c58372ea8e7d6085d57d205
Marek Wisniewski <a@a.pl>
2024-07-13 11:30:45 +0200

    data types

---
+++ myscript.js
@@ -0,0 +1,32 @@
+//Numeric
+let length = 180
+console.log(length)
+let weight = 85.5
+console.log(weight)
+
+
+//String
+let color = "Yellow"
+console.log(color)
+
+
+//Boolean
+let x = true
+console.log(x)
+let y = false
+console.log(y)
+
+
+//Object
+let person = {firstName:"Marek", lastName:"Wisniewski"}
+console.log(person)
+
+
+//Array
+let cars = ["Saab", "Volvo", "BMW"]
+console.log(cars)
+
+
+//Date
+const date = new Date("2024-08-31")
+console.log(date)
