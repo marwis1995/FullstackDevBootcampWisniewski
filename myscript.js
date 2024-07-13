cbbcbf455389b93529f37cdf1e305fb0f8fefcf5
Marek Wisniewski <a@a.pl>
2024-07-13 11:42:56 +0200

    comparison

--- myscript.js
+++ myscript.js
@@ -30,3 +30,35 @@ console.log(cars)
 //Date
 const date = new Date("2024-08-31")
 console.log(date)
+
+
+//OPERATORS
+
+//Equal
+console.log("Wynik porównania: " + (length == weight))
+
+//Equal value and equal type
+let length2 = "180"
+console.log(length === length2)
+
+//Not equal
+console.log(100 != 150)
+
+//Not eq value and type
+console.log(100 !== "100")
+
+//Greater
+console.log(100 > 99)
+
+//Less
+console.log(99 < 100)
+
+//Grater or eq
+console.log(100 >= 100)
+
+//Less or eq
+console.log(100 <= 100)
+
+//Ternary operator
+let result = 100 > 101 ? "TAK" : "NIE"
+console.log(result)
