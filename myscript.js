2a44041569534ba5f032fc5090f225a5c36a8bf3
Marek Wisniewski <a@a.pl>
2024-07-13 14:47:08 +0200

    test

--- myscript.js
+++ myscript.js
@@ -1,12 +1,38 @@
-window.onload = function (){
-    const button = document.querySelector('#actionButton')
-    const inputBox = document.querySelector('#inputBox')
-    const message = document.querySelector('#message')
+//Numeric
+let length = 180
+console.log(typeof length)
+console.log(length)
+let weight = 85.5
+console.log(weight)
 
 
-    message.innerHTML='<h1>aaaa</h1>'
-    button.addEventListener("click", function(){
-        // alert(inputBox.value)
-        message.innerHTML='<h1>aaaa</h1>'
-    })
-}
+//String
+let color = "Yellow"
+console.log(color)
+console.log(typeof color)
+
+
+//Boolean
+let x = true
+console.log(x)
+let y = false
+console.log(y)
+console.log(typeof y)
+
+
+//Object
+let person = {firstName:"Marek", lastName:"Wisniewski"}
+console.log(person)
+console.log(typeof person)
+
+
+//Array
+let cars = ["Saab", "Volvo", "BMW"]
+console.log(cars)
+console.log(typeof cars)
+
+
+//Date
+const date = new Date("2024-08-31")
+console.log(date) 
+console.log(typeof date)
