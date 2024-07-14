4f005207c754d6d8d3b85943e6436f28e57adb58
Marek Wisniewski <a@a.pl>
2024-07-14 09:22:11 +0200

    callback

--- myscript.js
+++ myscript.js
@@ -1,38 +1,14 @@
-//Numeric
-let length = 180
-console.log(typeof length)
-console.log(length)
-let weight = 85.5
-console.log(weight)
+function myDisplayer(text){
+    alert(text)
+}
 
+function myCalculator(num1, num2, myCallback){
+    let sum = num1 + num2
+    myCallback(sum)
+}
 
-//String
-let color = "Yellow"
-console.log(color)
-console.log(typeof color)
+myCalculator(5, 5, myDisplayer)
 
-
-//Boolean
-let x = true
-console.log(x)
-let y = false
-console.log(y)
-console.log(typeof y)
-
-
-//Object
-let person = {firstName:"Marek", lastName:"Wisniewski"}
-console.log(person)
-console.log(typeof person)
-
-
-//Array
-let cars = ["Saab", "Volvo", "BMW"]
-console.log(cars)
-console.log(typeof cars)
-
-
-//Date
-const date = new Date("2024-08-31")
-console.log(date) 
-console.log(typeof date)
+window.onload = function(){
+    myCalculator(5, 5, myDisplayer)
+}
