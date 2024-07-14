e12d2299aaf11d05a7b87634d07bb0d1360a68a9
Marek Wisniewski <a@a.pl>
2024-07-14 09:51:39 +0200

    Promise

--- myscript.js
+++ myscript.js
@@ -1,14 +1,23 @@
-function myDisplayer(text){
-    alert(text)
+
+function downloadSomeData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Data downloaded succesfully")
+            // reject("Error while data downloading")
+        }, 5000)
+    })
 }
 
-function myCalculator(num1, num2, myCallback){
-    let sum = num1 + num2
-    myCallback(sum)
-}
 
-myCalculator(5, 5, myDisplayer)
-
-window.onload = function(){
-    myCalculator(5, 5, myDisplayer)
-}
+downloadSomeData()
+    .then(result => {
+        console.log("Success")
+        console.log("Success")
+        console.log("Success")
+        console.log("Success")
+        console.log("Success")
+        console.log("Success")
+    })
+    .catch(error => {
+        console.log(error)
+    })
